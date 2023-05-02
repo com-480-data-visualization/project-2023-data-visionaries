87722767598aa780f1f271fe14146fe031347dd8
Vinko Sabolcec <vinko.sabolcec1@gmail.com>
2023-05-02 22:12:47 +0200

    Switch map to TopoJSON

--- website/src/D3WorldMapVisualisation/D3WorldMapVisualisation.jsx
+++ website/src/D3WorldMapVisualisation/D3WorldMapVisualisation.jsx
@@ -5,14 +5,18 @@ import * as d3 from "d3";
 import { useAtomValue } from 'jotai';
 import { yearAtom } from '../state';
 import { geoWinkel3 } from "https://cdn.skypack.dev/d3-geo-projection@4";
-import geoJsonUrl from "../data/custom.geo.json?url";
+import topoJsonUrl from "../data/countries-110m.json?url";
 import dataUrl from "../data/data_scores.csv?url";
+import iso3166CodesUrl from "../data/iso3166_codes.json?url";
+import * as tjson from "topojson-client";
 
 const D3WorldMapVisualisation = ({ width, height }) => {
+    const [scores, setScores] = useState(null);
+    const [topoJson, setTopoJson] = useState(null);
+    const [iso3166Codes, setIso3166Codes] = useState(null);
     const year = useAtomValue(yearAtom);
     const [hoveredCountry, setHoveredCountry] = useState("");
-    const [scores, setScores] = useState(null);
-    const [geoJson, setGeoJson] = useState(null);
+
     const svgRef = useRef();
     const gRef = useRef();
 
@@ -25,15 +29,28 @@ const D3WorldMapVisualisation = ({ width, height }) => {
         return ret;
     }
 
+    const processIso3166Codes = (codes) => {
+        const ret = {};
+        for (let code of codes) {
+            ret[code["country-code"]] = code["alpha-2"];
+        }
+        return ret;
+    }
+
     const getScore = (year, country_code) => {
         return scores[year][country_code];
     }
 
     useEffect(() => {
-        fetch(geoJsonUrl)
+        fetch(topoJsonUrl)
             .then(res => res.json())
-            .then(gj => setGeoJson(gj))
-            .catch(err => console.error("Error loading geoJson of countries.", err));
+            .then(tj => setTopoJson(tj))
+            .catch(err => console.error("Error loading topoJson of countries.", err));
+        fetch(iso3166CodesUrl)
+            .then(res => res.json())
+            .then(c => processIso3166Codes(c))
+            .then(c => setIso3166Codes(c))
+            .catch((err) => console.log("Error loading ISO-3166 codes", err));
         d3.csv(dataUrl)
             .then(scores => processScores(scores))
             .then(scores => setScores(scores))
@@ -56,42 +73,38 @@ const D3WorldMapVisualisation = ({ width, height }) => {
     }, []);
 
     useEffect(() => {
-        if (!scores || !geoJson) return;
+        if (!scores || !topoJson || !iso3166Codes) return;
         const gElement = d3.select(gRef.current);
 
         const projection = geoWinkel3();
         const pathGenerator = d3.geoPath().projection(projection);
 
-        const getColorByScore = d3
+        const colorByScore = d3
             .scaleLinear()
             .domain([3, 5, 6, 7, 8])
             .range(["red", "orange", "yellow", "green", "blue"]);
 
         const getFillColor = e => {
-            const score = getScore(year, e.properties.iso_a2_eh);
+            const score = getScore(year, iso3166Codes[e.id]);
             if (score == undefined) return '#bbb';
-            //return getColorByIndex(getIndex(year, e.properties.name));
-            return getColorByScore(score);
+            return colorByScore(score);
         }
 
-        gElement.selectAll('path').data(geoJson.features)
+        const countries = gElement
+            .selectAll('path')
+            .data(tjson.feature(topoJson, topoJson.objects.countries).features);
+
+        const countriesEnter = countries
             .enter()
             .append('path')
             .attr('d', pathGenerator);
 
-        gElement.selectAll('path')
-            .attr('fill', getFillColor)
+        countriesEnter
+            .merge(countries)
             .attr('class', style.country)
-            .attr('opacity', 1.0)
-            .on('mouseover', (e, d) => {
-                const countryName = d.properties.iso_a2_eh;
-                setHoveredCountry(countryName);
-            })
-            .on('mouseleave', d => {
-                setHoveredCountry("")
-            })
+            .attr('fill', getFillColor);
 
-    }, [year, scores, geoJson]);
+    }, [year, scores, topoJson, iso3166Codes]);
 
     return (
         <svg width={width} height={height} ref={svgRef} viewBox='0 0 400 200'>
