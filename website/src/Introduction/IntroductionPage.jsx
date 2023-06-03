b9505f33314ad3681732715a7552b6fe4f643c7d
marta <marta.knezevic10201@gmail.com>
2023-06-03 11:23:19 +0200

    update story

--- website/src/Introduction/IntroductionPage.jsx
+++ website/src/Introduction/IntroductionPage.jsx
@@ -6,15 +6,23 @@ const IntroductionPage = () => {
             <h2>World happiness report</h2>
             <div>
                 <p className={style.text}>Welcome to our World Happiness Visualization.</p>
-                <br /><br />Here you can:
-                <ul>
-                    <li>explore happiness levels all over the world</li>
-                    <li>find out which countries are the happiest</li>
-                    <li>compare the countries to their neighbours</li>
-                    <li>see how happiness changes over the time</li>
-                    <li>find out which factors contribute to countries' happiness</li>
-                    <li>find out is there a secret to being happy</li>
-                </ul>
+                <br />
+                <p className={style.text}>In this project, we created different visualizations for data from the “World Happiness Report”, publication that measures the subjective well-being of individuals across countries and regions. The data contains happiness score for world countries and the following additional measures: GDP per capita, healthy life expectancy, social support, freedom to make life choices, generosity and corruption. </p>
+
+
+                <div className={style.text}>
+                    <br /><br />Here you can:
+                    <ul>
+                        <li>explore happiness levels all over the world</li>
+                        <li>find out which countries are the happiest</li>
+                        <li>compare the countries to their neighbours</li>
+                        <li>see how happiness changes over the time</li>
+                        <li>find out which factors contribute to countries' happiness</li>
+                        <li>find out if there is a secret to being happy</li>
+                    </ul>
+                </div>
+                <br />
+                <p className={style.text}>Enjoy exploring our visualizations.</p>
             </div>
         </div>
     );
